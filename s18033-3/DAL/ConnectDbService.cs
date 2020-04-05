9d80a66cf246f0b11142dc86034af824a93f989f
Piotr Kwiatek <piotr.kwiatek@outlook.com>
2020-04-05 09:56:14 +0200

    Prepare for working with db

---
+++ s18033-3/DAL/ConnectDbService.cs
@@ -0,0 +1,35 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Cw3.DAL
+{
+    public class ConnectDbService : IDbService
+    {
+        private static string connectionString = "Data Source=db-mssql;Initial Catalog=s18033;Integrated Security=True";
+        private static IEnumerable<Student> _students;
+
+        static void MockDbService()
+        {
+            using (var client = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT FirstName FROM dbo.Student"))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            System.Diagnostics.Debug.WriteLine("{0}", reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
--- s18033-3/Startup.cs
+++ s18033-3/Startup.cs
@@ -25,7 +25,8 @@ namespace s18033_3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IDbService, MockDbService>();
+            services.AddSingleton<IDbService, ConnectDbService>();
+            // services.AddSingleton<IDbService, MockDbService>();
             services.AddControllers();
         }
 
